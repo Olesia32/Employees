0f983f43aa0482ee243deafa53b9f52ce4155110
ykarpa <yurii.karpa@lnu.edu.ua>
2023-03-06 19:11:55 +0200

    Створено клас, поля класу, конструктори

--- Employees/Employees/Employee.cs
+++ Employees/Employees/Employee.cs
@@ -8,5 +8,45 @@ namespace Employees
 {
     class Employee
     {
+        private string surname;
+        private double experience;
+        private double wage_per_hour;
+        private double minimum_amount_hour;
+
+        public string Surname
+        {
+            get { return surname; }
+            private set { surname = value; }
+        }
+        public double Experience
+        {
+            get { return experience; }
+            private set { experience = value; }
+        }
+        public double Wage_per_hour
+        {
+            get { return wage_per_hour; }
+            private set { wage_per_hour = value; }
+        }
+        public double Minimum_amount_hour
+        {
+            get { return minimum_amount_hour; }
+            private set { minimum_amount_hour = value; }
+        }
+
+        public Employee()
+        {
+            surname = string.Empty;
+            experience = 0.0;
+            wage_per_hour = 0.0;
+            minimum_amount_hour = 0.0;
+        }
+        public Employee(string _surname, double _experience, double _wage_per_hour, double _minimum_amount_hour)
+        {
+            surname = _surname;
+            experience = _experience;
+            wage_per_hour = _wage_per_hour;
+            minimum_amount_hour = _minimum_amount_hour;
+        }
     }
 }
--- Employees/Employees/Program.cs
+++ Employees/Employees/Program.cs
@@ -6,7 +6,9 @@ namespace Employees
     {
         static void Main(string[] args)
         {
-            
+            Employee e1= new Employee("koks", 20, 50, 40);
+            Employee e2 = new Employee();
+            Console.WriteLine(e2);
         }
     }
 }
